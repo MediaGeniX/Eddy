06a3fd968d45e2236e584f53e6fc8d1075ef1ef0
Tom Naessens <siloxtom@gmail.com>
2016-03-09 18:31:16 +0100

    Made a basic home page

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mgx1000km_session'
+Rails.application.config.session_store :cookie_store, key: '_eddy_session'
