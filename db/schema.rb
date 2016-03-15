fa54e00fde9217e94cae05bddcb1f24419ad999a
Toon Willems <willemstoon@gmail.com>
2016-03-15 21:22:24 +0100

    add trips to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315200753) do
+ActiveRecord::Schema.define(version: 20160315202042) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -19,12 +19,8 @@ ActiveRecord::Schema.define(version: 20160315200753) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "trips", force: :cascade do |t|
-    t.integer  "distance"
-    t.datetime "trip_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+# Could not dump table "trips" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
