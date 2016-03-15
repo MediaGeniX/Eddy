7301f974319d729643cb6720701c84f29ab033aa
Tom Naessens <Tom.Naessens@UGent.be>
2016-03-15 21:08:31 +0100

    Add location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315193922) do
+ActiveRecord::Schema.define(version: 20160315200753) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trips", force: :cascade do |t|
     t.integer  "distance"
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id         :integer          not null, primary key
+#  distance   :integer
+#  trip_date  :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TripTest < ActiveSupport::TestCase
