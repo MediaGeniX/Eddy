558b424046e4989538086c80fb547a6c7ca8368f
Tom Naessens <Tom.Naessens@UGent.be>
2016-03-15 20:59:29 +0100

    Fix registration forms and shit

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,9 @@ ActiveRecord::Schema.define(version: 20160313193827) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "age"
     t.integer  "country"
+    t.date     "birthday"
+    t.integer  "location",               default: 0
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
