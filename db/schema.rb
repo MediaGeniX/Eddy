8bd8a58dfdddc89e3cd64a7429dd51269f49295e
Toon Willems <willemstoon@gmail.com>
2016-03-15 20:41:51 +0100

    create trippy model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313193827) do
+ActiveRecord::Schema.define(version: 20160315193922) do
+
+  create_table "trips", force: :cascade do |t|
+    t.integer  "distance"
+    t.datetime "trip_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
