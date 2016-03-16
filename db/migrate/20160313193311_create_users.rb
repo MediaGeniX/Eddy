9c1425218ffea3626d8765b3ba953dfb7f0b1d8e
Tom Naessens <Tom.Naessens@UGent.be>
2016-03-16 23:00:00 +0100

    Clean up migrations

--- db/migrate/20160313193311_create_users.rb
+++ db/migrate/20160313193311_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name,      null: false
-      t.integer :country,  null: false
       t.date :birthdate,   null: false
       t.integer :location, null: false, default: 0
       t.boolean :admin,    null: false, default: false
