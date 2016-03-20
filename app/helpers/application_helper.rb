a32863c092446d195ada4d34fae7e7a9235a1065
Tom Naessens <Tom.Naessens@UGent.be>
2016-03-20 21:50:32 +0100

    Add progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def form_errors(object)
     render partial: 'form_errors', locals: { object: object }
   end
+
+  def meter_to_kilometer(meter)
+    meter / 1000.0
+  end
+
+  def pretty_number(number)
+    number_with_delimiter(number, delimiter: '.')
+  end
 end
