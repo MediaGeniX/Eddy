a772a8298e642b9634f765946b203fd81aab4730
Toon Willems <willemstoon@gmail.com>
2016-03-22 20:48:52 +0100

    basic tests, which don't actually test. more sanity

--- test/controllers/scoreboard_controller_test.rb
+++ test/controllers/scoreboard_controller_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class ScoreboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  include Devise::TestHelpers
+
+  test "should assign yellow jersey" do
+    get :index
+    assert assigns(:yellow_jersey)
+  end
+
+  test "should assign polka dot jersey" do
+    get :index
+    assert assigns(:polka_dot_jersey)
+  end
+
+  test "should assign green jersey" do
+    get :index
+    assert assigns(:green_jersey)
+  end
+
+  test "should assign pink jersey" do
+    get :index
+    assert assigns(:pink_jersey)
+  end
+
+  test "should assign white jersey" do
+    get :index
+    assert assigns(:white_jersey)
+  end
+
+  test "should assign grey jersey" do
+    get :index
+    assert assigns(:grey_jersey)
+  end
 end
