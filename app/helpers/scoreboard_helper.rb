b24034b976f2a56f82fd29b9c7db61a339720ad7
Toon Willems <willemstoon@gmail.com>
2016-03-22 19:56:03 +0100

    refactor card into helper and partial

--- app/helpers/scoreboard_helper.rb
+++ app/helpers/scoreboard_helper.rb
@@ -1,2 +1,22 @@
 module ScoreboardHelper
+
+  def yellow_yersey_card(yersey_data)
+    yersey_card data: yersey_data,
+      title: "Yellow Jersey",
+      sub_title: "Most total distance",
+      table_header: "Distance",
+      color_value: "ffff00",
+      data_block: Proc.new {|(user, meter)| pretty_number(meter_to_kilometer(meter))}
+  end
+
+  def yersey_card(data: data, title: title, sub_title: sub_title, table_header: table_header, color_value: color_value, data_block: data_block)
+    render partial: 'yersey_card',
+      locals: {data: data,
+               extract_block: data_block,
+               title: title,
+               sub_title: sub_title,
+               table_header: table_header,
+               color_value: color_value,
+              }
+  end
 end
