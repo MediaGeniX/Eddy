6740b0644c154b17296b9ee9a12b21d0c9661307
Tom Naessens <Tom.Naessens@UGent.be>
2017-03-09 20:37:30 +0100

    Correct git url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, "Eddy"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:MediaGeniX/Eddy.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
