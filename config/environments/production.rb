44f4961fca591b4fef1daa4c2d182e1b8ca7a48b
Tom Naessens <Tom.Naessens@UGent.be>
2017-03-10 20:38:10 +0100

    Some more mail stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { host: 'eddy.mediagenix.tv' }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'eddy.mediagenix.tv' }
 end
