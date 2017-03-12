b4efc30a424bdcfd5b8b80a7df9832a0c1d5569d
Tom Naessens <Tom.Naessens@UGent.be>
2017-03-12 12:16:44 +0100

    Add slackistrano

--- Gemfile
+++ Gemfile
@@ -75,6 +75,7 @@ group :development do
   gem 'capistrano-rails', '~> 1.2'
   gem 'capistrano-rvm'
   gem 'capistrano-passenger'
+  gem 'slackistrano'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- config/deploy.rb
+++ config/deploy.rb
@@ -31,3 +31,9 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+set :slackistrano, {
+ channel: '#eddy',
+ webhook: 'https://hooks.slack.com/services/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'
+}
+
