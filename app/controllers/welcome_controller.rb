3d24bb8ea34fdf9151067bb815c413b3d507a32e
Tom Naessens <Tom.Naessens@UGent.be>
2017-03-19 15:18:40 +0100

    Fix yellow jersey on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @yellow_jersey = Trip.group(:user).order('sum_distance_in_meter DESC').sum(:distance_in_meter)
+    season = Season.default
+    @yellow_jersey = Trip.where(trip_date: season.start_date..season.end_date).group(:user).order('sum_distance_in_meter DESC').sum(:distance_in_meter)
   end
 end
