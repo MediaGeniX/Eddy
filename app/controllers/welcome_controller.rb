638688268c680d878a2420848565a163f3b62658
Tom Naessens <Tom.Naessens@UGent.be>
2017-03-19 15:29:20 +0100

    Fix tests a cleaner way

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     season = Season.default
-    start_date = season ? season.start_date : Date.new(1900)
-    end_date = season ? season.end_date : Date.new(2200)
-    @yellow_jersey = Trip.where(trip_date: start_date..end_date).group(:user).order('sum_distance_in_meter DESC').sum(:distance_in_meter)
+    @yellow_jersey = Trip.where(trip_date: season.start_date..season.end_date).group(:user).order('sum_distance_in_meter DESC').sum(:distance_in_meter)
   end
 end
